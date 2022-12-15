7402a8b0d69c39ecdcc8169ea0eb6fdbf43d8f67
RegulineTrain72 <60439606+RegulineTrain72@users.noreply.github.com>
2022-12-15 10:48:16 +0100

    Add files via upload

---
+++ zh2/zh2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using zh2;
+
+List<engiszi> list = new List<engiszi>();
+int size = 3628;
+
+for (int i = 0; i < size; i++)
+{
+    list.Add(new engiszi());
+}
+
+int sum = 0;
+int optimist = 0;
+int ordinary = 0;
+int pessimist = 0;
+
+foreach (engiszi engiszi in list)
+{
+    sum += engiszi.mood;
+    switch (engiszi.type)
+    {
+        case "ordinary":
+            ordinary++;
+            break;
+        case "optimist":
+            optimist++;
+            break;
+        case "pessimist":
+            pessimist++;
+            break;
+
+    }
+}
+Console.WriteLine("összboldogság: " + sum);
+Console.WriteLine("A bolygó népessége: " + size);
+Console.WriteLine("optimista: " + ((float)optimist / size) * 100 + "% , pesszimista: " + ((float)pessimist / size) * 100 + "% , átlagos: " + ((float)ordinary / size) * 100 + "%");
---
+++ zh2/zh2/engiszi.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zh2
+{
+    internal class engiszi
+    {
+        public string type { get; set; }
+        public int mood { get; set; }
+
+        public engiszi()
+        {
+            Random rnd = new Random();
+            float k = (float)(rnd.NextDouble() * 100);
+            if (k <= 20)
+            {
+                this.type = "pessimist";
+            }
+            else if (k > 20 && k <= 87)
+            {
+                this.type = "ordinary";
+            }
+            else
+            {
+                this.type = "optimist";
+            }
+            if (type == "ordinary")
+            {
+                this.mood = (int)(rnd.NextDouble() * 100);
+            }
+            else if (type == "pessimist")
+            {
+                this.mood = (int)(rnd.NextDouble() * 20);
+            }
+            else
+                this.mood = (int)(rnd.NextDouble() * 40);
+        }
+    }
+}
